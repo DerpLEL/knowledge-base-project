d9c674a8f939e5e67400ebbacc526cbef0bf0ae5
DerpLEL <honguyenngocbao@gmail.com>
2024-03-08 00:59:50 +0700

    Forgot to add utf-8

--- config.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-
-class DefaultConfig:
-    OPENAI_API_BASE_GPT4_TURBO = 'aHR0cHM6Ly9vcGVuYWktbm9pcy1pbnRlcm4tMi5vcGVuYWkuYXp1cmUuY29tLw=='
-    DEPLOYMENT_NAME_GPT4_TURBO = 'gpt-4-turbo'
-    OPENAI_API_KEY_GPT4_TURBO = 'NjU0NjdlZjZmYjA2NDBlZDhkOWNkNTcxZTA0OTc4YWU='
