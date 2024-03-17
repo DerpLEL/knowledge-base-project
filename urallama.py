bc7e82867dcefde814778224d63edc65b3c303a0
DerpLEL <honguyenngocbao@gmail.com>
2024-03-17 15:45:40 +0700

    Add local Llama (URALlama API is down)

--- urallama.py
+++ urallama.py
@@ -34,8 +34,36 @@ class URAWrapper:
         return -1
 
 
-# model = URAWrapper()
-#
-# print(model.predict(
-#     "[INST] What day comes after Saturday? [/INST]"
-# ))
+class URALocalWrapper:
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    def predict(self, input: str):
+        data = {
+            "model": "ura-llama-7b",
+            "prompt": input,
+            "stream": False,
+            "stop": ["</s>", "[INST]", "/.", "[INST:"],
+            "temperature": 0.1,
+            "top_k": 50,
+            "top_p": 0.9,
+        }
+
+        answer = requests.post(
+            url='http://127.0.0.1:5000/v1/completions',
+            headers=self.headers,
+            data=json.dumps(data)
+        )
+
+        if answer.status_code == 200:
+            return answer.json()['choices'][0]['text'].strip()
+
+        return -1
+
+
+model = URALocalWrapper()
+
+print(model.predict(
+    "[INST] Sau thứ bảy là thứ mấy? [/INST]"
+))
