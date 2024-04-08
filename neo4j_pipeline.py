7d437487ed4669014fe095b823945b95d898d298
DerpLEL <honguyenngocbao@gmail.com>
2024-04-09 02:37:04 +0700

    Add neo4j db and pipeline thingy

---
+++ neo4j_pipeline.py
@@ -0,0 +1,168 @@
+from neo4j import GraphDatabase
+import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
+
+GOOGLE_API_KEY = 'AIzaSyAnT0-DpdDE63wJpH51BT3GiB1n8e_tFNo'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+URI = "neo4j://localhost:7687"
+AUTH = ("neo4j", "jupiter-club-edition-manila-papa-9630")
+
+driver = GraphDatabase.driver(URI, auth=AUTH)
+
+driver.verify_connectivity()
+
+
+def retriever(query: str):
+    prompt = """Given a user query, generate a Neo4j's Cypher database query to get subgraph(s).
+
+Database entities:
+Intent (attribute: name:"Rút môn học")
+Student (attribute: id)
+Course (attribute: name)
+Semester (attribute: name)
+
+Database relations:
+(Student)-[:CÓ_Ý_ĐỊNH]->(Intent)
+(Intent)-[:ĐANG_YÊU_CẦU_RÚT]->(Course)
+(Course)-[:TRONG_HỌC_KỲ]->(Semester)
+
+User query: {query}
+Cypher query: """
+
+    answer = model.generate_content(
+        prompt.format(
+            query=query
+        ),
+        generation_config=genai.types.GenerationConfig(
+            temperature=0.0,
+        ),
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        }
+    ).text
+    print(answer)
+
+    processed_query = '\n'.join(answer.split('\n')[1:-1])
+
+    records, summary, keys = driver.execute_query(
+        processed_query,
+        database_="neo4j",
+    )
+
+    result = []
+    for i in records:
+        data = i.data()
+        if data not in result:
+            result.append(data)
+
+    # print('Triples:', triples)
+
+    return result
+
+
+def llm_context(query: str):
+    prompt = '''Given a user query, generate context related to the query from your knowledge.
+    
+User query: {query}
+Generated context: '''
+
+    answer = model.generate_content(
+        prompt.format(
+            query=query
+        ),
+        generation_config=genai.types.GenerationConfig(
+            temperature=0.5,
+        ),
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        }
+    ).text
+
+    return answer
+
+
+def fuse_context(triples, llm_context):
+    variable_mapping = {
+        's': 'Student',
+        'i': 'Intent',
+        'c': 'Course',
+        'sem': 'Semester'
+    }
+
+    relations_left = {
+        's': '[:CÓ_Ý_ĐỊNH]',
+        'i': '[:ĐANG_YÊU_CẦU_RÚT]',
+        'c': '[:TRONG_HỌC_KỲ]'
+    }
+
+    relations_right = {
+        '[:CÓ_Ý_ĐỊNH]': 'i',
+        '[:ĐANG_YÊU_CẦU_RÚT]': 'c',
+        '[:TRONG_HỌC_KỲ]': 'sem'
+    }
+
+    text = ''
+    for index, i in enumerate(triples, 1):
+        text += f'Case {index}:\n'
+        text += f'''Student {i['s']} -[:CÓ_Ý_ĐỊNH]-> Intent {i['i']}
+Intent {i['i']} -[:ĐANG_YÊU_CẦU_RÚT]-> Course {i['c']}
+Course {i['c']} -[:TRONG_HỌC_KỲ]-> Semester {i["sem"]}
+'''
+
+
+    template = f'''<General information>
+{llm_context}
+    
+<Specific cases>
+{text}'''
+
+    return template
+
+
+def final_answer(query: str, context: str):
+    prompt = '''Given a user query and context, answer the question.
+
+Context:
+{context}
+
+User query: {query}
+Answer: '''
+
+    answer = model.generate_content(
+        prompt.format(
+            context=context,
+            query=query
+        ),
+        generation_config=genai.types.GenerationConfig(
+            temperature=0.0,
+        ),
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        }
+    ).text
+
+    return answer
+
+
+sample_query = 'Em muốn rút môn Sức bền vật liệu cho học kì 212'
+
+triples = retriever(sample_query)
+print(triples)
+generated_context = llm_context(sample_query)
+print(generated_context)
+fused = fuse_context(triples, generated_context)
+
+print("Final answer:", final_answer(sample_query, fused))
