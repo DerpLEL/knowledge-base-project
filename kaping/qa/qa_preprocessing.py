6f078296f4ccd9d42fe084660d5594755e9b699c
DerpLEL <honguyenngocbao@gmail.com>
2024-05-23 10:03:20 +0700

    Add function for loading MetaQA

--- kaping/qa/qa_preprocessing.py
+++ kaping/qa/qa_preprocessing.py
@@ -5,6 +5,16 @@ This contains a simple script to load the Mintaka dataset in json-file
 
 import json
 import random
+import re
+
+string = "which person directed the movies starred by [John Krasinski]"
+match = re.search(r'[(.*?)]', string)
+
+if match:
+    result = match.group(1)
+    print(result)
+else:
+    print("No match found")
 
 
 class Pair:
@@ -64,3 +74,84 @@ def load_webqsp():
         ))
 
     return qa_pairs
+
+
+def load_metaqa():
+    qa_pairs = []
+
+    three_hops = []
+    two_hops = []
+
+    with open("../metaqa-2-hop.txt", 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+        prev_answer = ""
+        prev_line = lines[0]
+
+        for i in lines[1:]:
+            if "[" not in i:
+                prev_answer += i.strip()
+
+            else:
+                question, answers = prev_line.split('\t')
+                answers += prev_answer
+
+                answers = answers.strip().split('|')
+                res = re.search(r'\[(.*?)]', question)
+                if res:
+                    entity = [res.group(1)]
+
+                dct = {
+                    "question": question.replace("[", '').replace(']', ''),
+                    "answers": answers,
+                    "entities": entity,
+                    "type": '2-hop'
+                }
+
+                two_hops.append(dct)
+                prev_line = i
+
+    with open("../metaqa-3-hop.txt", 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+        prev_answer = ""
+        prev_line = lines[0]
+
+        for i in lines[1:]:
+            if "[" not in i:
+                prev_answer += i.strip()
+
+            else:
+                question, answers = prev_line.split('\t')
+                answers += prev_answer
+
+                answers = answers.strip().split('|')
+                res = re.search(r'\[(.*?)]', question)
+                if res:
+                    entity = [res.group(1)]
+
+                dct = {
+                    "question": question.replace("[", '').replace(']', ''),
+                    "answers": answers,
+                    "entities": entity,
+                    "type": '3-hop'
+                }
+
+                three_hops.append(dct)
+                prev_line = i
+
+    random.seed(27)
+    chosen_set = random.choices(two_hops, k=10) + random.choices(three_hops, k=10)
+
+    for data in chosen_set:
+        qa_pairs.append(Pair(
+            question=data['question'],
+            entities=data['entities'],
+            answer=data['answers'],
+            q_type=data['type']
+        ))
+
+    return qa_pairs
+
+
+print(load_metaqa())
